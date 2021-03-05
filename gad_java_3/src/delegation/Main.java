dbf7197d441075717e0f055b7f3fee51b02e0916
andreeatatulescu <andreea.tatulescu2610@gmail.com>
2021-03-05 18:05:17 +0200

    design patterns in class

---
+++ gad_java_3/src/delegation/Main.java
@@ -0,0 +1,10 @@
+package lab3.delegation;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        PayCalculator p = new PayCalculator();
+        e.calculatePay(p);
+    }
+
+}
