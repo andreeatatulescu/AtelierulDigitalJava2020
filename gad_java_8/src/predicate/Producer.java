055acc102f8a376d60b5de77967251effc0b0f3f
Andreea Tătulescu <56994777+andreeatatulescu@users.noreply.github.com>
2021-03-05 23:47:38 +0200

    Update Producer.java

--- gad_java_8/src/predicate/Producer.java
+++ gad_java_8/src/predicate/Producer.java
@@ -17,12 +17,23 @@ public class Producer {
     @Test
     public void predicate2(){
         Predicate<String> p1 = s-> s!=null;
-        Predicate<String> p2=s->!s.isEmpty();
-        Predicate<String> p3=  p1.and(p2);
+        Predicate<String> p2 = s->!s.isEmpty();
+        Predicate<String> p3 = p1.and(p2);
 
         Assert.assertFalse(p3.test(""));
         Assert.assertFalse(p3.test(null));
         Assert.assertTrue(p3.test("dsadas"));
 
+    }
+    
+    @Test
+    public void predicate3(){
+        Predicate<String> p1 = s->s.length() == 4;
+        Predicate<String> p2 = s->s.startsWith("J");
+        Predicate<String> p3 = p1.or(p2);
+
+        Assert.assertTrue(p3.test("True"));
+        Assert.assertTrue(p3.test("Julia"));
+        Assert.assertTrue(p3.test("Java"));
     }
 }
